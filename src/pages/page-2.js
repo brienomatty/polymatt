d8555e5ee6dcc5092cac2d938d64d3b1fe7a53c2
Matthew O'Brien <matthewobrien@Matthews-MBP.fios-router.home>
2019-03-03 10:50:01 -0500

    Tweaked some language on second page - part 2

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two - my second blog post (coming soon!)" />
-    <h1>Hi from the second page</h1>
+    <h1>Hi from the second page (next blog post coming soon!)</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
